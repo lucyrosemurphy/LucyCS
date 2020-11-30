66f3a752eda7066e4c292adc7bfed338cbbe26a8
lucyrosemurphy <74979154+lucyrosemurphy@users.noreply.github.com>
2020-11-29 19:02:39 -0500

    This is a tester that was provided by
    https://www.websapex.com/blog/tutorial/php/simple-html-contact-form-to-send-email-in-php/

--- contact.php
+++ contact.php
@@ -1,44 +1,64 @@
 <?php
+    if(isset($_POST['submit']))
+    {
+        $name = $_POST['name']; // Get Name value from HTML Form
+        $email_id = $_POST['email']; // Get Email Value
+        $mobile_no = $_POST['mobile']; // Get Mobile No
+        $msg = $_POST['message']; // Get Message Value
 
-if($_POST["submit"]) {
-    $recipient="your@email.address";
-    $subject="Form to email message";
-    $sender=$_POST["sender"];
-    $senderEmail=$_POST["senderEmail"];
-    $message=$_POST["message"];
+        $to = "lucyrosemurphy@gmail.com"; // You can change here your Email
+        $subject = "'$name' has been sent a mail"; // This is your subject
 
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+        // HTML Message Starts here
+        $message ="
+        <html>
+            <body>
+                <table style='width:600px;'>
+                    <tbody>
+                        <tr>
+                            <td style='width:150px'><strong>Name: </strong></td>
+                            <td style='width:400px'>$name</td>
+                        </tr>
+                        <tr>
+                            <td style='width:150px'><strong>Email ID: </strong></td>
+                            <td style='width:400px'>$email_id</td>
+                        </tr>
+                        <tr>
+                            <td style='width:150px'><strong>Mobile No: </strong></td>
+                            <td style='width:400px'>$mobile_no</td>
+                        </tr>
+                        <tr>
+                            <td style='width:150px'><strong>Message: </strong></td>
+                            <td style='width:400px'>$msg</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </body>
+        </html>
+        ";
+        // HTML Message Ends here
 
-    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+        // Always set content-type when sending HTML email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-    $thankYou="<p>Thank you! Your message has been sent.</p>";
-}
+        // More headers
+        $headers .= 'From: Admin <admin@websapex.com>' . "\r\n"; // Give an email id on which you want get a reply. User will get a mail from this email id
+        $headers .= 'Cc: info@websapex.com' . "\r\n"; // If you want add cc
+        $headers .= 'Bcc: boss@websapex.com' . "\r\n"; // If you want add Bcc
 
-?><!DOCTYPE html>
+        if(mail($to,$subject,$message,$headers)){
+            // Message if mail has been sent
+            echo "<script>
+                    alert('Mail has been sent Successfully.');
+                </script>";
+        }
 
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Contact form to email</title>
-</head>
-
-<body>
-
-    <?=$thankYou ?>
-
-    <form method="post" action="contact.php">
-        <label>Name:</label>
-        <input name="sender">
-
-        <label>Email address:</label>
-        <input name="senderEmail">
-
-        <label>Message:</label>
-        <textarea rows="5" cols="20" name="message"></textarea>
-
-        <input type="submit" name="submit">
-    </form>
-
-</body>
-
-</html>
+        else{
+            // Message if mail has been not sent
+            echo "<script>
+                    alert('EMAIL FAILED');
+                </script>";
+        }
+    }
+?>
